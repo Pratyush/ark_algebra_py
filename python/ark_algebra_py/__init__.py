1e43bab5b4c136785d5ec14b451add0c6c374c1b
Pratyush Mishra <pratyushmishra@berkeley.edu>
2023-09-26 05:58:00 -0400

    Generate documentation automatically

--- python/ark_algebra_py/__init__.py
+++ python/ark_algebra_py/__init__.py
@@ -1,6 +1,7 @@
 import ark_algebra_py
-from ark_algebra_py import ark_algebra_py
+ark_algebra_py = ark_algebra_py.ark_algebra_py
 
 __doc__ = ark_algebra_py.ark_algebra_py.__doc__
-if hasattr(ark_algebra_py.ark_algebra_py, "__all__"):
-    __all__ = ark_algebra_py.ark_algebra_py.__all__
+from ark_algebra_py.ark_algebra_py import G1, G2, GT, Scalar, Polynomial, Domain, Pairing
+if hasattr(ark_algebra_py, "__all__"):
+    __all__ = ark_algebra_py.__all__
