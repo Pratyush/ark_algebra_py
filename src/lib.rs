4722345ce4937053ac8cd4c3bceedbe8b54d64df
Pratyush Mishra <pratyushmishra@berkeley.edu>
2023-09-26 04:27:01 -0400

    Add exponentiation for GT

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod polynomial;
 pub(crate) mod utils;
 
 use pyo3::prelude::*;
-use wrapper::{Domain, Pairing, PairingOutput, Polynomial, Scalar, G1, G2};
+use wrapper::{Domain, Pairing, GT, Polynomial, Scalar, G1, G2};
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -16,7 +16,7 @@ fn ark_algebra_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<G1>()?;
     m.add_class::<G2>()?;
     m.add_class::<Pairing>()?;
-    m.add_class::<PairingOutput>()?;
+    m.add_class::<GT>()?;
     m.add_class::<Polynomial>()?;
     m.add_class::<Domain>()?;
 
--- src/pairing.rs
+++ src/pairing.rs
@@ -5,10 +5,10 @@ macro_rules! monomorphize_pairing {
 
         #[derive(Copy, Clone)]
         #[pyo3::pyclass]
-        pub struct PairingOutput(POutput<$inner>);
+        pub struct GT(POutput<$inner>);
 
         #[pyo3::pymethods]
-        impl PairingOutput {
+        impl GT {
             #[new]
             pub fn generator() -> Self {
                 use ark_ec::Group;
@@ -23,6 +23,11 @@ macro_rules! monomorphize_pairing {
             // Overriding operators
             fn __mul__(&self, rhs: Self) -> Self {
                 Self(self.0 + rhs.0)
+            }
+
+            fn __pow__(&self, other: Scalar, _modulo: Option<u128>) -> Self {
+                use std::ops::Mul;
+                Self(self.0.mul(other.0))
             }
 
             pub fn inverse(&self) -> Self {
@@ -56,169 +61,22 @@ macro_rules! monomorphize_pairing {
         #[pymethods]
         impl $struct {
             #[staticmethod]
-            fn multi_pairing(py: Python, g1s: Vec<$g1>, g2s: Vec<$g2>) -> PairingOutput {
+            fn multi_pairing(py: Python, g1s: Vec<$g1>, g2s: Vec<$g2>) -> GT {
                 py.allow_threads(|| {
                     let g1_inner: Vec<ark_bls12_381::G1Affine> =
                         g1s.into_par_iter().map(|g1| g1.0.to_affine()).collect();
                     let g2_inner: Vec<ark_bls12_381::G2Affine> =
                         g2s.into_par_iter().map(|g2| g2.0.to_affine()).collect();
-                    PairingOutput(<$inner>::multi_pairing(g1_inner, g2_inner))
+                    GT(<$inner>::multi_pairing(g1_inner, g2_inner))
                 })
             }
 
             #[staticmethod]
-            fn pairing(py: Python, g1: $g1, g2: $g2) -> PairingOutput {
+            fn pairing(py: Python, g1: $g1, g2: $g2) -> GT {
                 py.allow_threads(|| {
-                    PairingOutput(<$inner>::pairing(g1.0.to_affine(), g2.0.to_affine()))
+                    GT(<$inner>::pairing(g1.0.to_affine(), g2.0.to_affine()))
                 })
             }
         }
     };
 }
-
-// pub struct Pair<P: Pairing>(PhantomData<P>);
-
-// #[derive(Copy, Clone)]
-// #[pyclass]
-// pub struct GT(PairingOutput<ark_bls12_381::Bls12_381>);
-
-// #[pymethods]
-// impl GT {
-//     #[new]
-//     fn generator() -> GT {
-//         GT(PairingOutput::generator())
-//     }
-
-//     #[staticmethod]
-//     fn one() -> GT {
-//         GT(PairingOutput::zero())
-//     }
-
-//     // Overriding operators
-//     fn __mul__(&self, rhs: GT) -> GT {
-//         // The group operation is multiplication of the underlying field, but
-//         // the group is written additively
-//         GT(self.0 + rhs.0)
-//     }
-
-//     fn inverse(&self) -> GT {
-//         // The group operation is multiplication of the underlying field, but
-//         // the group is written additively
-//         GT(-self.0)
-//     }
-//     fn __str__(&self) -> PyResult<String> {
-//         let mut bytes = Vec::new();
-//         self.0
-//             .serialize_compressed(&mut bytes)
-//             .map_err(serialisation_error_to_py_err)?;
-//         Ok(hex::encode(bytes))
-//     }
-//     fn __richcmp__(&self, other: GT, op: pyclass::CompareOp) -> PyResult<bool> {
-//         match op {
-//             pyclass::CompareOp::Eq => Ok(self.0 == other.0),
-//             pyclass::CompareOp::Ne => Ok(self.0 != other.0),
-//             _ => Err(exceptions::PyValueError::new_err(
-//                 "comparison operator not implemented".to_owned(),
-//             )),
-//         }
-//     }
-// }
-
-// #[macro_export]
-// macro_rules! monomorphize_field {
-//     ($struct: ident, $inner: ty, $COMPRESSED_SIZE: expr) => {
-//         use ark_ff::Field;
-//         use pyo3::{PyResult, pyclass, exceptions};
-
-//         use crate::utils::serialisation_error_to_py_err;
-
-//         #[derive(Copy, Clone)]
-//         #[pyclass]
-//         pub struct $struct($inner);
-
-//         #[pymethods]
-//         impl $struct {
-//             fn new(integer: u128) -> Self {
-//                 Self(<$inner>::from(integer))
-//             }
-
-//             fn zero() -> Self {
-//                 Self(<$inner>::zero())
-//             }
-
-//             fn one() -> Self {
-//                 Self(<$inner>::one())
-//             }
-
-//             // Overriding operators
-//             fn __add__(&self, rhs: Self) -> Self {
-//                 Self(self.0 + rhs.0)
-//             }
-
-//             fn __sub__(&self, rhs: Self) -> Self {
-//                 Self(self.0 - rhs.0)
-//             }
-
-//             fn __mul__(&self, rhs: Self) -> Self {
-//                 Self(self.0 * rhs.0)
-//             }
-
-//             fn __neg__(&self) -> Self {
-//                 Self(-self.0)
-//             }
-
-//             fn __str__(&self) -> PyResult<String> {
-//                 return Ok(hex::encode(self.to_le_bytes()?));
-//             }
-
-//             fn __richcmp__(&self, other: Self, op: pyclass::CompareOp) -> PyResult<bool> {
-//                 match op {
-//                     pyclass::CompareOp::Eq => Ok(self.0 == other.0),
-//                     pyclass::CompareOp::Ne => Ok(self.0 != other.0),
-//                     _ => Err(exceptions::PyValueError::new_err(
-//                         "comparison operator not implemented".to_owned(),
-//                     )),
-//                 }
-//             }
-
-//             fn square(&self) -> Self {
-//                 Self(self.0.square())
-//             }
-
-//             fn double(&self) -> Self {
-//                 Self(self.0.double())
-//             }
-
-//             fn inverse(&self) -> Self {
-//                 Self(self.0.inverse().unwrap_or_default())
-//             }
-
-//             fn batch_inverse(elems: Vec<Self>) -> Vec<Self> {
-//                 let mut elems = elems.into_iter().map(|e| e.0).collect::<Vec<_>>();
-//                 ark_ff::batch_inversion(&mut elems);
-//                 elems.into_iter().map(Self).collect()
-//             }
-
-//             fn is_zero(&self) -> bool {
-//                 self.0.is_zero()
-//             }
-
-//             fn is_one(&self) -> bool {
-//                 self.0.is_one()
-//             }
-
-//             fn to_le_bytes<const N: usize>(&self) -> PyResult<[u8; N]> {
-//                 let mut bytes = [0u8; N];
-//                 self.0
-//                     .serialize_compressed(&mut bytes[..])
-//                     .map_err(serialisation_error_to_py_err)
-//                     .map(|_| bytes)
-//             }
-
-//             fn from_le_bytes<const N: usize>(bytes: [u8; N]) -> PyResult<Self> {
-//                 <$inner>::deserialize_compressed(&bytes[..])
-//                     .map_err(serialisation_error_to_py_err).map(Self)
-//             }
-//         }
-//     }
-// }
